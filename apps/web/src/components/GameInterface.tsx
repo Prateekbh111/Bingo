5a87feccbef799051f93a99d41e0d4e7f42bc46d
Prateek Bhardwaj <prateekbh111@gmail.com>
2024-10-07 16:29:42 +0530

    configured vercel.json

--- apps/web/src/components/GameInterface.tsx
+++ apps/web/src/components/GameInterface.tsx
@@ -66,7 +66,7 @@ export default function GameInterface({
 
 	useEffect(() => {
 		const newSocket = new WebSocket(
-			`ws://${process.env.NEXT_PUBLIC_WEB_SOCKET_URL}:8080/token=${sessionToken}`,
+			`wss://${process.env.NEXT_PUBLIC_WEB_SOCKET_URL}/ws/${sessionToken}`,
 		);
 		newSocket.onopen = () => console.log("Connection established");
 		newSocket.onmessage = handleSocketMessage;

f9f4c611f2cb69a445b3913efc43e8c73af4cd7e
Prateek Bhardwaj <prateekbh111@gmail.com>
2025-09-06 00:56:48 +0530

    added env in turbo.json and nexxt.confi.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
 	reactStrictMode: false,
 	publicRuntimeConfig: {
 		NEXT_PUBLIC_WEB_SOCKET_URL: process.env.NEXT_PUBLIC_WEB_SOCKET_URL,
+		NEXT_PUBLIC_WS_PORT: process.env.NEXT_PUBLIC_WS_PORT,
 	},
 };
 
